0dafa116403c69d9975fc5475d6abcb49023e085
David Leonel <davidx6x@hotmail.com>
2021-11-03 20:32:35 -0300

    login con autenticacion deÃl proyecto final

---
+++ routes/admin/login.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+var usuariosModel = require('./../../models/usuariosModel');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('admin/login', {
+        layout: 'admin/layout'
+    });
+});
+
+
+router.post('/', async (req, res, next) => {
+    try {
+        var usuario = req.body.user;
+        var clave = req.body.password;
+
+        var data = await usuariosModel.getUserByUsernameAndPassword(usuario, clave);
+
+        if (data != undefined) {
+            req.session.id = data.id;
+            req.session.user = data.user;
+            
+            res.redirect('/admin/novedades');
+        } else {
+            res.render('admin/login', {
+                layout: 'admin/layout',
+                error: true
+            });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+
+router.get('/logout', function (req, res, next) {
+    req.session.destroy();
+    res.render('admin/login', {
+        layout: 'admin/layout'
+    });
+});
+
+module.exports = router;
