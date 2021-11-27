62563ae6aba88ac9585432ae459449bfbe6776a6
David Leonel <davidx6x@hotmail.com>
2021-11-26 21:54:25 -0300

    se genera el envio de mails

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var noticiasModel = require('./../models/noticiasModel');
 var cloudinary = require('cloudinary').v2;
+var nodemailer = require('nodemailer');
 
 
 /* GET home page. */
@@ -30,6 +31,35 @@ router.get('/novedades', async function (req, res, next) {
     });
 
     res.json(noticias);
+});
+
+
+
+/* Contacto */
+router.post('/contacto', async (req, res) => {
+    const mail = {
+        to: 'davidx6x@hotmail.com',
+        subject: 'Contacto desde la página web',
+        html: `${req.body.nombre}  se contactó a traves de la web y solicita más información a éste correo:  ${req.body.email} <br>
+        Su teléfono es ${req.body.telefono}  y te ha dejado éste mensaje: ${req.body.mensaje}`
+    }
+
+
+    const transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    });
+
+    await transport.sendMail(mail)
+
+    res.status(201).json({
+        error: false,
+        message:  'Mensaje enviado con exito!'
+    });
 });
 
 
